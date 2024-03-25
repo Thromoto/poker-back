e3d7ea1bdce329a0a7bdbd9f54539369cc08fb8e
Thromoto <thi.omoto@gmail.com>
2024-03-25 17:38:02 -0300

    x

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import db from "./src/database/db.js";
 import routes from "./src/routes/routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
