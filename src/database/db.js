1d799e5b72da933b9c6564df352c1fbcb626ca4d
Thromoto <thi.omoto@gmail.com>
2024-03-26 16:36:58 -0300

    ttt

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import 'dotenv/config';
 
-const db = mongoose.connect(process.env.MONGO_DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const db = mongoose.connect(process.env.MONGO_DB_URL, {});
 
 export default db;
