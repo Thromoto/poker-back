1c9e5e06b2811dfdcf7b82bfb52e015f0b542e3a
Thromoto <thi.omoto@gmail.com>
2024-03-26 18:33:43 -0300

    ccc

--- api/index.js
+++ api/index.js
@@ -12,17 +12,26 @@ app.use(express.json());
 
 app.use(routes);
 
+// Esta é a função que a Vercel espera que você exporte
 export default async function handler(req, res) {
-  // Se necessário, você pode adicionar lógica aqui para manipular as solicitações recebidas pelo servidor.
-  res.status(200).json({ message: "API está funcionando corretamente." });
+  try {
+    // Antes de lidar com a solicitação, aguarde a conexão com o banco de dados
+    await db.connect();
+
+    // Roteie a solicitação para as rotas definidas na sua aplicação
+    app(req, res);
+  } catch (error) {
+    console.error("Erro ao processar solicitação:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
 }
 
-// O código abaixo inicia o servidor Express, mas isso é apenas um exemplo. A Vercel ignora essa parte e usa a função exportada acima.
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}).catch((error) => {
-  console.log("Failed to connect to the database:", error);
-  process.exit(1); // Encerra o processo, indicando um erro
-});
+// // O código abaixo inicia o servidor Express, mas isso é apenas um exemplo. A Vercel ignora essa parte e usa a função exportada acima.
+// db.then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+//   });
+// }).catch((error) => {
+//   console.log("Failed to connect to the database:", error);
+//   process.exit(1); // Encerra o processo, indicando um erro
+// });
